df222158f0f93f05f058bd53b22d265e99ab1898
Soslan Aldatov <soslanaldatov@gmail.com>
2024-06-12 16:54:32 +0400

    WIP: check new Dockerfile

--- freeswitch/entrypoint.sh
+++ freeswitch/entrypoint.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-# Set environment variables
-DB_HOST=${DB_HOST:-localhost}
-DB_PORT=${DB_PORT:-5432}
-DB_NAME=${DB_NAME:-fusionpbx}
-DB_USERNAME=${DB_USERNAME:-fusionpbx}
-DB_PASSWORD=${DB_PASSWORD:-fusionpbx}
+# # Set environment variables
+# DB_HOST=${DB_HOST:-localhost}
+# DB_PORT=${DB_PORT:-5432}
+# DB_NAME=${DB_NAME:-fusionpbx}
+# DB_USERNAME=${DB_USERNAME:-fusionpbx}
+# DB_PASSWORD=${DB_PASSWORD:-fusionpbx}
 
-ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-adminpass}
-DOMAIN_NAME=${DOMAIN_NAME:-example.com}
+# ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
+# ADMIN_PASSWORD=${ADMIN_PASSWORD:-adminpass}
+# DOMAIN_NAME=${DOMAIN_NAME:-example.com}
 
-FS_HOST=${FS_HOST:-localhost}
+# FS_HOST=${FS_HOST:-localhost}
 
-# Set the FusionPBX database configuration in the database initializing file
-sed -i "s|{admin_username}|${ADMIN_USERNAME}|" /var/www/fusionpbx/db-init.php
-sed -i "s|{admin_password}|${ADMIN_PASSWORD}|" /var/www/fusionpbx/db-init.php
-sed -i "s|{domain_name}|${DOMAIN_NAME}|" /var/www/fusionpbx/db-init.php
-sed -i "s|{database_host}|${DB_HOST}|" /var/www/fusionpbx/db-init.php
-sed -i "s|{database_port}|${DB_PORT}|" /var/www/fusionpbx/db-init.php
-sed -i "s|{database_name}|${DB_NAME}|" /var/www/fusionpbx/db-init.php
-sed -i "s|{database_username}|${DB_USERNAME}|" /var/www/fusionpbx/db-init.php
-sed -i "s|{database_password}|${DB_PASSWORD}|" /var/www/fusionpbx/db-init.php
+# # Set the FusionPBX database configuration in the database initializing file
+# sed -i "s|{admin_username}|${ADMIN_USERNAME}|" /var/www/fusionpbx/db-init.php
+# sed -i "s|{admin_password}|${ADMIN_PASSWORD}|" /var/www/fusionpbx/db-init.php
+# sed -i "s|{domain_name}|${DOMAIN_NAME}|" /var/www/fusionpbx/db-init.php
+# sed -i "s|{database_host}|${DB_HOST}|" /var/www/fusionpbx/db-init.php
+# sed -i "s|{database_port}|${DB_PORT}|" /var/www/fusionpbx/db-init.php
+# sed -i "s|{database_name}|${DB_NAME}|" /var/www/fusionpbx/db-init.php
+# sed -i "s|{database_username}|${DB_USERNAME}|" /var/www/fusionpbx/db-init.php
+# sed -i "s|{database_password}|${DB_PASSWORD}|" /var/www/fusionpbx/db-init.php
 
-# Update FS directories permissions
-/bin/chmod -R 777 /usr/local/freeswitch/conf
-/bin/chmod -R 777 /usr/local/freeswitch/sounds
-/bin/chmod -R 777 /usr/local/freeswitch/db
-/bin/chmod -R 777 /usr/local/freeswitch/recordings
-/bin/chmod -R 777 /usr/local/freeswitch/storage
-/bin/chmod -R 777 /usr/local/freeswitch/scripts
+# # Update FS directories permissions
+# /bin/chmod -R 777 /usr/local/freeswitch/conf
+# /bin/chmod -R 777 /usr/local/freeswitch/sounds
+# /bin/chmod -R 777 /usr/local/freeswitch/db
+# /bin/chmod -R 777 /usr/local/freeswitch/recordings
+# /bin/chmod -R 777 /usr/local/freeswitch/storage
+# /bin/chmod -R 777 /usr/local/freeswitch/scripts
 
-/bin/chmod -R 777 /etc/fusionpbx
-/bin/chown -R www-data:www-data /etc/fusionpbx
+# /bin/chmod -R 777 /etc/fusionpbx
+# /bin/chown -R www-data:www-data /etc/fusionpbx
 
-# Initialize the FusionPBX database
-/usr/bin/php /var/www/fusionpbx/db-init.php
+# # Initialize the FusionPBX database
+# /usr/bin/php /var/www/fusionpbx/db-init.php
 
 # Start FreeSWITCH and Apache
 /usr/local/freeswitch/bin/freeswitch -nonat -nf -nc &
