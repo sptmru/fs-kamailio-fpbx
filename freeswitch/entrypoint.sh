20b267b283227e12b335660e779562fc3d6cdd8e
Soslan Aldatov <soslanaldatov@gmail.com>
2024-06-12 17:23:16 +0400

    updaate entrypoint

--- freeswitch/entrypoint.sh
+++ freeswitch/entrypoint.sh
@@ -39,6 +39,6 @@
 
 # Start FreeSWITCH and Apache
 # /usr/local/freeswitch/bin/freeswitch -nonat -nf -nc &
-freeswitch -nonat -nf -nc &
+/usr/bin/freeswitch -nonat -nf -nc
 # source /etc/apache2/envvars
 # exec apache2 -DFOREGROUND
